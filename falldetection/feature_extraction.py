df0d89ee379df63a61ea7466b37e88d81c33be69
frankknoll <Knoll_Frank@web.de>
2018-07-02 20:59:16 +0200

    refactoring

--- falldetection/feature_extraction.py
+++ falldetection/feature_extraction.py
@@ -1,5 +1,4 @@
-def time_window(df, window_center_index, window_size):
-    half_window_size = int(window_size / 2)
+def time_window(df, window_center_index, half_window_size):
     lower_bound = window_center_index - half_window_size
     upper_bound = window_center_index + half_window_size + 1
     return df[lower_bound:upper_bound].reset_index(drop=True)
--- falldetection/test_feature_extraction.py
+++ falldetection/test_feature_extraction.py
@@ -9,10 +9,10 @@ class FeatureExtractionTestCase(TestCase):
 
     def test_time_window(self):
         df = pd.DataFrame({'Acc_X': [1.0, 2.0, 3.0, 4.0, 5.0]})
-        df_time_windowed = time_window(df, window_center_index=2, window_size=5)
+        df_time_windowed = time_window(df, window_center_index=2, half_window_size=2)
         self.assertTrue(df.equals(df_time_windowed))
 
     def test_time_window2(self):
         df = pd.DataFrame({'Acc_X': [1.0, 2.0, 3.0, 4.0, 5.0]})
-        df_time_windowed = time_window(df, window_center_index=2, window_size=3)
+        df_time_windowed = time_window(df, window_center_index=2, half_window_size=1)
         self.assertTrue(pd.DataFrame({'Acc_X': [2.0, 3.0, 4.0]}).equals(df_time_windowed))
