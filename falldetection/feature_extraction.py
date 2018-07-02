ecdec76b6f1f82d59ad576d6f372b6390abb7a88
frankknoll <Knoll_Frank@web.de>
2018-07-02 21:46:46 +0200

    refining FeatureExtractionTestCase

--- falldetection/feature_extraction.py
+++ falldetection/feature_extraction.py
@@ -4,3 +4,8 @@ def time_window(df, window_center_index, half_window_size):
     if not (0 <= lower_bound < len(df) and 0 <= upper_bound <= len(df)):
         raise IndexError()
     return df[lower_bound:upper_bound].reset_index(drop=True)
+
+
+def get_index_of_maximum_total_acceleration(df):
+    squared_total_acceleration = df['Acc_X'] ** 2 + df['Acc_Y'] ** 2 + df['Acc_Z'] ** 2
+    return squared_total_acceleration.idxmax()
--- falldetection/test_feature_extraction.py
+++ falldetection/test_feature_extraction.py
@@ -2,12 +2,12 @@ from unittest import TestCase
 
 import pandas as pd
 
-from falldetection.feature_extraction import time_window
+from falldetection.feature_extraction import *
 
 
 class FeatureExtractionTestCase(TestCase):
 
-    def test_time_window(self):
+    def test_time_window1(self):
         df = pd.DataFrame({'Acc_X': [1.0, 2.0, 3.0, 4.0, 5.0]})
         df_time_windowed = time_window(df, window_center_index=2, half_window_size=2)
         self.assertTrue(df.equals(df_time_windowed))
@@ -21,3 +21,19 @@ class FeatureExtractionTestCase(TestCase):
         df = pd.DataFrame({'Acc_X': [1.0, 2.0, 3.0, 4.0, 5.0]})
         with self.assertRaises(IndexError):
             time_window(df, window_center_index=2, half_window_size=3)
+
+    def test_get_index_of_maximum_total_acceleration1(self):
+        df = pd.DataFrame(
+            {'Acc_X': [1.0, 2.0, 30.0, 4.0, 5.0],
+             'Acc_Y': [1.0, 2.0, 40.0, 4.0, 5.0],
+             'Acc_Z': [1.0, 2.0, 50.0, 4.0, 5.0]})
+        index = get_index_of_maximum_total_acceleration(df)
+        self.assertEquals(index, 2)
+
+    def test_get_index_of_maximum_total_acceleration2(self):
+        df = pd.DataFrame(
+            {'Acc_X': [1.0, 20.0, 3.0, 4.0, 5.0],
+             'Acc_Y': [1.0, 30.0, 4.0, 4.0, 5.0],
+             'Acc_Z': [1.0, 40.0, 5.0, 4.0, 5.0]})
+        index = get_index_of_maximum_total_acceleration(df)
+        self.assertEquals(index, 1)
