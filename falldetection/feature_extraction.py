9234b2d8ccee72cf4431c4a8feab30fda7c0982a
frankknoll <Knoll_Frank@web.de>
2018-07-02 21:20:53 +0200

    refining FeatureExtractionTestCase

--- falldetection/feature_extraction.py
+++ falldetection/feature_extraction.py
@@ -1,4 +1,6 @@
 def time_window(df, window_center_index, half_window_size):
     lower_bound = window_center_index - half_window_size
     upper_bound = window_center_index + half_window_size + 1
+    if not (0 <= lower_bound < len(df) and 0 <= upper_bound <= len(df)):
+        raise IndexError()
     return df[lower_bound:upper_bound].reset_index(drop=True)
--- falldetection/test_feature_extraction.py
+++ falldetection/test_feature_extraction.py
@@ -16,3 +16,8 @@ class FeatureExtractionTestCase(TestCase):
         df = pd.DataFrame({'Acc_X': [1.0, 2.0, 3.0, 4.0, 5.0]})
         df_time_windowed = time_window(df, window_center_index=2, half_window_size=1)
         self.assertTrue(pd.DataFrame({'Acc_X': [2.0, 3.0, 4.0]}).equals(df_time_windowed))
+
+    def test_time_window3(self):
+        df = pd.DataFrame({'Acc_X': [1.0, 2.0, 3.0, 4.0, 5.0]})
+        with self.assertRaises(IndexError):
+            time_window(df, window_center_index=2, half_window_size=3)
