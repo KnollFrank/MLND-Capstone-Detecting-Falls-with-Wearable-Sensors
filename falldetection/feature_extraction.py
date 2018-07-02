3ec5f50c1528cb256332275633df014f2f05f82a
frankknoll <Knoll_Frank@web.de>
2018-07-02 12:34:01 +0200

    refactoring

--- falldetection/feature_extraction.py
+++ falldetection/feature_extraction.py
@@ -1,5 +1,5 @@
-def time_window(df, midpoint_index, window_size):
+def time_window(df, window_center_index, window_size):
     half_window_size = int(window_size / 2)
-    lower_bound = midpoint_index - half_window_size
-    upper_bound = midpoint_index + half_window_size + 1
+    lower_bound = window_center_index - half_window_size
+    upper_bound = window_center_index + half_window_size + 1
     return df[lower_bound:upper_bound].reset_index(drop=True)
