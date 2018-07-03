3d61a7e7a1a85511941ff92245a0de9ba18b7cf0
frankknoll <Knoll_Frank@web.de>
2018-07-03 23:09:54 +0200

    refactoring

--- falldetection/test_feature_extraction.py
+++ falldetection/test_feature_extraction.py
@@ -7,22 +7,6 @@ from falldetection.sensor_files_provider import SensorFilesProvider
 
 class FeatureExtractionTestCase(TestCase):
 
-    def test_slice_with_window1(self):
-        df = pd.DataFrame({'Acc_X': [1.0, 2.0, 3.0, 4.0, 5.0]})
-        df_sliced = slice_with_window(df, window_center_index=2, half_window_size=2)
-        self.assertTrue(df.equals(df_sliced))
-
-    def test_slice_with_window2(self):
-        df = pd.DataFrame({'Acc_X': [1.0, 2.0, 3.0, 4.0, 5.0]})
-        df_sliced_actual = slice_with_window(df, window_center_index=2, half_window_size=1)
-        df_sliced_expected = pd.DataFrame({'Acc_X': [2.0, 3.0, 4.0]}, index=[1, 2, 3])
-        self.assertTrue(df_sliced_expected.equals(df_sliced_actual))
-
-    def test_slice_with_window3(self):
-        df = pd.DataFrame({'Acc_X': [1.0, 2.0, 3.0, 4.0, 5.0]})
-        with self.assertRaises(IndexError):
-            slice_with_window(df, window_center_index=2, half_window_size=3)
-
     def test_get_index_of_maximum_total_acceleration1(self):
         df = pd.DataFrame(
             {'Acc_X': [1.0, 2.0, 30.0, 4.0, 5.0],
