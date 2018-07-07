ac49748fa09d84f7a9f9c5fc2b68e2bf7a752557
frankknoll <Knoll_Frank@web.de>
2018-07-07 19:55:30 +0200

    refactoring

--- falldetection/tests/test_extract_features.py
+++ falldetection/tests/test_extract_features.py
@@ -4,25 +4,21 @@ import pandas as pd
 from statsmodels.tsa.stattools import acovf
 
 from falldetection.extract_features import extract_features
-from falldetection.sensor_file_reader import read_sensor_file
 
 
-# TODO: alle features (autocorrelation und DFT) implementieren.
+# TODO: alle features (DFT) implementieren.
 
 
 class ExtractFeaturesTestCase(TestCase):
 
-    # TODO: DRY with test_extract_features2
-    def test_extract_features1(self):
+    def __test_extract_features(self, autocorr_num, add_expected_autocorr_of_df_2_features):
         df = pd.DataFrame(
             {'Acc_X': [1.0, 2.0, 3.0],
              'Gyr_X': [4.0, 5.0, 6.0]})
-        features_actual = extract_features(df, autocorr_num=1)
+        features_actual = extract_features(df, autocorr_num)
         print("features_actual:\n", features_actual)
 
-        features_expected = pd.DataFrame(
-            columns=['Acc_X', 'Gyr_X'],
-            dtype=pd.np.float64)
+        features_expected = pd.DataFrame(columns=['Acc_X', 'Gyr_X'], dtype=pd.np.float64)
         features_expected.at['min', 'Acc_X'] = 1.0
         features_expected.at['min', 'Gyr_X'] = 4.0
 
@@ -46,53 +42,27 @@ class ExtractFeaturesTestCase(TestCase):
 
         features_expected.loc['kurtosis', :] = df.kurtosis()[features_expected.columns].values
 
-        features_expected.at['autocorr_lag_1', 'Acc_X'] = acovf(df['Acc_X'])[1]
-        features_expected.at['autocorr_lag_1', 'Gyr_X'] = acovf(df['Gyr_X'])[1]
+        add_expected_autocorr_of_df_2_features(df=df, features=features_expected)
         print("features_expected:\n", features_expected)
 
         self.assertTrue(features_expected.equals(features_actual))
 
+    def test_extract_features1(self):
+        def add_expected_autocorr_of_df_2_features(df, features):
+            features.at['autocorr_lag_1', 'Acc_X'] = acovf(df['Acc_X'])[1]
+            features.at['autocorr_lag_1', 'Gyr_X'] = acovf(df['Gyr_X'])[1]
+
+        self.__test_extract_features(
+            autocorr_num=1,
+            add_expected_autocorr_of_df_2_features=add_expected_autocorr_of_df_2_features)
+
     def test_extract_features2(self):
-        df = pd.DataFrame(
-            {'Acc_X': [1.0, 2.0, 3.0],
-             'Gyr_X': [4.0, 5.0, 6.0]})
-        features_actual = extract_features(df, autocorr_num=2)
-        print(features_actual)
+        def add_expected_autocorr_of_df_2_features(df, features):
+            features.at['autocorr_lag_1', 'Acc_X'] = acovf(df['Acc_X'])[1]
+            features.at['autocorr_lag_2', 'Acc_X'] = acovf(df['Acc_X'])[2]
+            features.at['autocorr_lag_1', 'Gyr_X'] = acovf(df['Gyr_X'])[1]
+            features.at['autocorr_lag_2', 'Gyr_X'] = acovf(df['Gyr_X'])[2]
 
-        features_expected = pd.DataFrame(
-            columns=['Acc_X', 'Gyr_X'],
-            dtype=pd.np.float64)
-        features_expected.at['min', 'Acc_X'] = 1.0
-        features_expected.at['min', 'Gyr_X'] = 4.0
-
-        features_expected.at['max', 'Acc_X'] = 3.0
-        features_expected.at['max', 'Gyr_X'] = 6.0
-
-        mean_Acc_X = (1.0 + 2.0 + 3.0) / 3
-        features_expected.at['mean', 'Acc_X'] = mean_Acc_X
-        mean_Gyr_X = (4.0 + 5.0 + 6.0) / 3
-        features_expected.at['mean', 'Gyr_X'] = mean_Gyr_X
-
-        features_expected.at['var', 'Acc_X'] = ((1.0 - mean_Acc_X) ** 2 + (2.0 - mean_Acc_X) ** 2 + (
-                3.0 - mean_Acc_X) ** 2) / 3
-        features_expected.at['var', 'Gyr_X'] = ((4.0 - mean_Gyr_X) ** 2 + (5.0 - mean_Gyr_X) ** 2 + (
-                6.0 - mean_Gyr_X) ** 2) / 3
-
-        features_expected.at['skew', 'Acc_X'] = ((1.0 - mean_Acc_X) ** 3 + (2.0 - mean_Acc_X) ** 3 + (
-                3.0 - mean_Acc_X) ** 3) / (3 * features_expected.at['var', 'Acc_X'] ** 3)
-        features_expected.at['skew', 'Gyr_X'] = ((4.0 - mean_Gyr_X) ** 3 + (5.0 - mean_Gyr_X) ** 3 + (
-                6.0 - mean_Gyr_X) ** 3) / (3 * features_expected.at['var', 'Gyr_X'] ** 3)
-
-        features_expected.loc['kurtosis', :] = df.kurtosis()[features_expected.columns].values
-
-        features_expected.at['autocorr_lag_1', 'Acc_X'] = acovf(df['Acc_X'])[1]
-        features_expected.at['autocorr_lag_2', 'Acc_X'] = acovf(df['Acc_X'])[2]
-        features_expected.at['autocorr_lag_1', 'Gyr_X'] = acovf(df['Gyr_X'])[1]
-        features_expected.at['autocorr_lag_2', 'Gyr_X'] = acovf(df['Gyr_X'])[2]
-
-        self.assertTrue(features_expected.equals(features_actual))
-
-    def test_extract_features3(self):
-        df = read_sensor_file("../../data/FallDataSet/101/Testler Export/901/Test_1/340535.txt")
-        features = extract_features(df, autocorr_num=1)
-        print("\n", features)
+        self.__test_extract_features(
+            autocorr_num=2,
+            add_expected_autocorr_of_df_2_features=add_expected_autocorr_of_df_2_features)
