2383e83e2543c025e6b011098d3c0b4313485a99
frankknoll <Knoll_Frank@web.de>
2018-07-10 22:45:54 +0200

    refactoring

--- falldetection/tests/test_feature_extractor_workflow.py
+++ falldetection/tests/test_feature_extractor_workflow.py
@@ -13,29 +13,29 @@ class FeatureExtractorWorkflowTestCase(TestCase):
 
     def test_extract_features1(self):
         # GIVEN
-        feature_extractor = FeatureExtractorWorkflow(lambda sensorFile: {
-            '../../data/FallDataSet-Test/209/Testler Export/914/Test_1/340535.txt': pd.DataFrame(data=[[1.0, 1.1]],
-                                                                                                 columns=['feature_0',
-                                                                                                          'feature_1']),
-            '../../data/FallDataSet-Test/209/Testler Export/914/Test_6/340535.txt': pd.DataFrame(data=[[2.0, 2.2]],
-                                                                                                 columns=['feature_0',
-                                                                                                          'feature_1']),
-            '../../data/FallDataSet-Test/209/Testler Export/801/Test_1/340535.txt': pd.DataFrame(data=[[3.0, 3.3]],
-                                                                                                 columns=['feature_0',
-                                                                                                          'feature_1']),
-            '../../data/FallDataSet-Test/209/Testler Export/801/Test_2/340535.txt': pd.DataFrame(data=[[4.0, 4.4]],
-                                                                                                 columns=['feature_0',
-                                                                                                          'feature_1']),
-            '../../data/FallDataSet-Test/101/Testler Export/801/Test_1/340535.txt': pd.DataFrame(data=[[5.0, 5.5]],
-                                                                                                 columns=['feature_0',
-                                                                                                          'feature_1']),
-            '../../data/FallDataSet-Test/101/Testler Export/801/Test_2/340535.txt': pd.DataFrame(data=[[6.0, 6.6]],
-                                                                                                 columns=['feature_0',
-                                                                                                          'feature_1']),
-            '../../data/FallDataSet-Test/101/Testler Export/920/Test_1/340535.txt': pd.DataFrame(data=[[7.0, 7.7]],
-                                                                                                 columns=['feature_0',
-                                                                                                          'feature_1'])}[
-            sensorFile])
+        feature_extractor = FeatureExtractorWorkflow(
+            lambda sensorFile: {
+                '../../data/FallDataSet-Test/209/Testler Export/914/Test_1/340535.txt':
+                    pd.DataFrame(data=[[1.0, 1.1]], columns=['feature_0', 'feature_1']),
+
+                '../../data/FallDataSet-Test/209/Testler Export/914/Test_6/340535.txt':
+                    pd.DataFrame(data=[[2.0, 2.2]], columns=['feature_0', 'feature_1']),
+
+                '../../data/FallDataSet-Test/209/Testler Export/801/Test_1/340535.txt':
+                    pd.DataFrame(data=[[3.0, 3.3]], columns=['feature_0', 'feature_1']),
+
+                '../../data/FallDataSet-Test/209/Testler Export/801/Test_2/340535.txt':
+                    pd.DataFrame(data=[[4.0, 4.4]], columns=['feature_0', 'feature_1']),
+
+                '../../data/FallDataSet-Test/101/Testler Export/801/Test_1/340535.txt':
+                    pd.DataFrame(data=[[5.0, 5.5]], columns=['feature_0', 'feature_1']),
+
+                '../../data/FallDataSet-Test/101/Testler Export/801/Test_2/340535.txt':
+                    pd.DataFrame(data=[[6.0, 6.6]], columns=['feature_0', 'feature_1']),
+
+                '../../data/FallDataSet-Test/101/Testler Export/920/Test_1/340535.txt':
+                    pd.DataFrame(data=[[7.0, 7.7]], columns=['feature_0', 'feature_1'])}
+            [sensorFile])
 
         sensor_files = SensorFilesProvider(baseDir='../../data/FallDataSet-Test',
                                            sensor=Sensor.WAIST).provide_sensor_files()
@@ -61,7 +61,6 @@ class FeatureExtractorWorkflowTestCase(TestCase):
         print('features_expected:\n', features_expected)
         print('features_actual:\n', features_actual)
         self.assertTrue(features_expected.equals(features_actual))
-
 
     @unittest.SkipTest
     def test_extract_features_and_save_WAIST(self):
