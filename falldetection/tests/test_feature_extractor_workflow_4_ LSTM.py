4cf743a6f7d5502271ff52f33021c2e751130c5d
frankknoll <Knoll_Frank@web.de>
2018-07-10 17:40:52 +0200

    refactoring

--- falldetection/tests/test_feature_extractor_workflow_4_ LSTM.py
+++ falldetection/tests/test_feature_extractor_workflow_4_ LSTM.py
@@ -10,58 +10,48 @@ from falldetection.sensor import Sensor
 
 class FeatureExtractorWorkflow4LSTMTestCase(TestCase):
 
-    # TODO: DRY with test_extract_features2
     def test_extract_features1(self):
-        # GIVEN
-        sensor_files = ['../../data/FallDataSet-Test/209/Testler Export/914/Test_1/340535.txt',
-                        '../../data/FallDataSet-Test/209/Testler Export/814/Test_6/340535.txt']
-
-        feature_extractor_4_lstm = FeatureExtractorWorkflow4LSTM(
-            sensor_file_2_df=lambda sensorFile: {
-                sensor_files[0]:
+        self.__test_extract_features(
+            df_by_sensor_file={
+                '../../data/FallDataSet-Test/209/Testler Export/914/Test_1/340535.txt':
                     pd.DataFrame({'Acc_X': [1.0, 2.0, 3.0], 'Acc_Y': [4.0, 5.0, 6.0]}),
 
-                sensor_files[1]:
-                    pd.DataFrame({'Acc_X': [10.0, 20.0, 30.0], 'Acc_Y': [40.0, 50.0, 60.0]})}
-            [sensorFile],
-            columns=['Acc_X', 'Acc_Y'])
+                '../../data/FallDataSet-Test/209/Testler Export/814/Test_6/340535.txt':
+                    pd.DataFrame({'Acc_X': [10.0, 20.0, 30.0], 'Acc_Y': [40.0, 50.0, 60.0]})},
+            columns=['Acc_X', 'Acc_Y'],
+            X_expected=np.array([[[1.0, 4.0],
+                                  [2.0, 5.0],
+                                  [3.0, 6.0]],
 
-        # WHEN
-        X_actual, y_actual = feature_extractor_4_lstm.extract_features(sensor_files)
-
-        # THEN
-        X_expected = np.array([[[1.0, 4.0],
-                                [2.0, 5.0],
-                                [3.0, 6.0]],
-
-                               [[10.0, 40.0],
-                                [20.0, 50.0],
-                                [30.0, 60.0]]])
-        y_expected = np.array([[True], [False]])
-        self.assertEquals(X_expected.tolist(), X_actual.tolist())
-        self.assertEquals(y_expected.tolist(), y_actual.tolist())
+                                 [[10.0, 40.0],
+                                  [20.0, 50.0],
+                                  [30.0, 60.0]]]),
+            y_expected=np.array([[True], [False]]))
 
     def test_extract_features2(self):
-        # GIVEN
-        sensor_files = ['../../data/FallDataSet-Test/209/Testler Export/814/Test_1/340535.txt',
-                        '../../data/FallDataSet-Test/209/Testler Export/814/Test_6/340535.txt']
-
-        feature_extractor_4_lstm = FeatureExtractorWorkflow4LSTM(
-            sensor_file_2_df=lambda sensorFile: {
-                sensor_files[0]:
+        self.__test_extract_features(
+            df_by_sensor_file={
+                '../../data/FallDataSet-Test/209/Testler Export/814/Test_1/340535.txt':
                     pd.DataFrame({'Acc_X': [1.0], 'Mag_X': [4.0]}),
 
-                sensor_files[1]:
-                    pd.DataFrame({'Acc_X': [10.0], 'Mag_X': [40.0]})}
-            [sensorFile],
-            columns=['Mag_X', 'Acc_X'])
+                '../../data/FallDataSet-Test/209/Testler Export/814/Test_6/340535.txt':
+                    pd.DataFrame({'Acc_X': [10.0], 'Mag_X': [40.0]})},
+            columns=['Mag_X', 'Acc_X'],
+            X_expected=np.array([[[4.0, 1.0]], [[40.0, 10.0]]]),
+            y_expected=np.array([[False], [False]]))
+
+    def __test_extract_features(self, df_by_sensor_file, columns, X_expected, y_expected):
+        # GIVEN
+        feature_extractor_4_lstm = FeatureExtractorWorkflow4LSTM(
+            sensor_file_2_df=df_by_sensor_file.get,
+            columns=columns)
+
+        sensor_files = df_by_sensor_file.keys()
 
         # WHEN
         X_actual, y_actual = feature_extractor_4_lstm.extract_features(sensor_files)
 
         # THEN
-        X_expected = np.array([[[4.0, 1.0]], [[40.0, 10.0]]])
-        y_expected = np.array([[False], [False]])
         self.assertEquals(X_expected.tolist(), X_actual.tolist())
         self.assertEquals(y_expected.tolist(), y_actual.tolist())
 
@@ -70,6 +60,7 @@ class FeatureExtractorWorkflow4LSTMTestCase(TestCase):
         features = extract_features_4_LSTM(
             sensor=Sensor.RIGHT_THIGH,
             baseDir='../../data/FallDataSet',
+            # TODO: DRY: genau dieselben sensor_files_to_exclude gibt es auch in test_feature_extractor_workflow.py und im Jupyter Notebook
             sensor_files_to_exclude=['208/Testler Export/805/Test_1/340539.txt',
                                      '203/Testler Export/813/Test_1/340539.txt',
                                      '103/Testler Export/911/Test_5/340539.txt',
